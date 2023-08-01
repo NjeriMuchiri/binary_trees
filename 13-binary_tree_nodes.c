2983d6d161e5c066827cb149a08ffdc2e7077fa1
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-02 00:00:42 +0300

    binary tree nodees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,12 +8,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
-    if (tree)
-    {
-        nodes += (tree->left || tree->right) ? 1 : 0;
-        nodes += binary_tree_nodes(tree->left);
-        nodes += binary_tree_nodes(tree->right);
-    }
-    return (nodes);
+size_t nodes = 0;
+if (tree)
+{
+nodes += (tree->left || tree->right) ? 1 : 0;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+}
+return (nodes);
 }

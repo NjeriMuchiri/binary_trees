bb0273fdd2eca5530de5559b22d101710c1b3c3f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-02 00:10:45 +0300

    binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures the balance
+ * factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance
  * factor
  * Return: 0 if tree is NULL
